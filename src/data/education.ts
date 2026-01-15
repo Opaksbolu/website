32dca1b0381448f86e281fabb7ab6f4d95ee63d1
Bolu Opakunle <opaksbolu@gmail.com>
2026-01-15 12:41:26 -0600

    Adding

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,7 +1,7 @@
 export const education = [
-  { school: "UniversityName", 
-    degree: "DegreeName", 
-    period: "TimePeriod", 
-    honors: "Honors" 
+  { school: "University of Texas at Dallas", 
+    degree: "Bachelor of Science in Computer Science", 
+    period: "2023 - 2025", 
+    honors: "GPA: 3.8" 
   },
 ];
