6c831827b4147c6027cc8b2718a7bf1827862aab
Bolu Opakunle <opaksbolu@gmail.com>
2026-01-16 16:43:22 -0600

    Adding

--- src/data/education.ts
+++ src/data/education.ts
@@ -1,7 +1,16 @@
 export const education = [
+  { school: "University of North Texas", 
+    degree: "Masters of Science in Data Science", 
+    period: "2026 - 2027", 
+    honors: "N/A" 
+  },
   { school: "University of Texas at Dallas", 
     degree: "Bachelor of Science in Computer Science", 
     period: "2023 - 2025", 
-    honors: "GPA: 3.8" 
+    honors: "N/A" 
+  },{ school: "Collin County Community College", 
+    degree: "Associate of Science in General Studies", 
+    period: "2021 - 2023", 
+    honors: "N/A" 
   },
 ];
