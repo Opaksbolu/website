40882fbd977c89aba40cfa341a796dfb80ab484e
Bolu Opakunle <opaksbolu@gmail.com>
2026-01-16 21:42:23 -0600

    Adding

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,7 +2,25 @@ export const experience = [
   { company: "Dexian", 
     role: "Access Verfication Analyst", 
     period: "Aug 2025 — Oct 2025", 
-    desc: "Developed, designed, and deployed software applications.", 
-    tags: ["Effective Communication", "Python", "MongoDB", "HTML", "Tailwind CSS", "JavaScript", "React"] 
+    desc: "Verification of roles, Removal of access, Scheduled follow-ups regarding roles, Emailed Supervisors regarding verification, Subitted weekly reports to management, Submitted tickets for removal.", 
+    tags: ["Access Verification", "Role Auditing", "Ticketing Systems"] 
   },
+  { company: "Mr. Cooper", 
+    role: "Internal IT Audit Intern", 
+    period: "Jun 2024 — Aug 2024", 
+    desc: "Assisted in documenting the tests for the design and operating effectiveness of IT SOX and SOC controls, including Manage Change, Manage Access, and IT Operations processes, Supported the evaluation of Continuous Controls Confirmation (C3) exceptions and validated issues, Performed various ad-hoc duties throughout different phases of the audit process, gaining a comprehensive understanding of internal auditing, Learned to prepare audit findings and recommendations that effectively communicate risks, focusing on their impact on the business, root causes, and key audit issues from the perspectives of senior management and the Audit Committee, Built and maintained strong relationships with management, internal auditors, and external auditors, Worked collaboratively with fellow interns to identify opportunities for process improvement in the organization and offered valuable recommendations for consideration", 
+    tags: ["Microsoft Excel", "Microsoft PowerPoint", "Teamwork", "Networking", "Troubleshooting","Computer Hardware Troubleshooting", "Project Planning", "Collaborative Problem Solving", "Remote Troubleshooting", "IT Audit", "Collaboration", "Collaborative Leadership", "Data Collection", "Volunteering", "Microsoft Word"] 
+  },
+  { company: "The Sport Source", 
+    role: "Data Entry Intern", 
+    period: "Jun 2023 — Mar 2024", 
+    desc: "Conducted daily research and entered data for all sports, colleges, and universities into the database, Explored and discussed more efficient work methods to improve productivity, Engaged with students and visitors, providing guidance on college and university education, Revised and reviewed previously entered data for accuracy, Assisted in coordinating the Sports Source showcase event, Proposed better practices to ensure a smoother and more accurate information verification process.", 
+    tags: ["Creativity", "Web Applications", "Databases", "Academic Advising", "Spreadsheets", "Analytical Skills", "Hardware", "Common Sense", "Data Entry", "Research Skills", "Data Analysis", "Troubleshooting", "Information Technology", "Collaborative Problem Solving", "Typing", "Problem Solving", "Data Collection", "Microsoft Office", "Computer Hardware", "Administrative Assistance"] 
+  },
+  { company: "Collin County Community College", 
+    role: "Student Assistant", 
+    period: "Mar 2022 — Aug 2023", 
+    desc: "Conducted assessment of resume-builder software platforms, Developed and implemented strategies utilizing digital tools, such as social media and email, to engage students, Researched and analyzed best practices from other career centers and created recommendations for improving Collin College's career center offerings, Compiled data and prepared monthly reports on the usage of the career center, Collaborated with Collin College staff to deliver high-quality events for students and employers, Troubleshoot technical issues by coordinating with technical support to assist students in achieving success, Served as a brand ambassador by combining your passion for people and technology to support our customers, Learned and utilized the Banner System to facilitate admissions including providing pre-academic advising to students and contacting students to remind them about registration deadlines, Created both audio and visual materials for the college to assist students.", 
+    tags: ["Customer Service", "Technical Support", "Event Planning", "Research Skills", "Data Analysis", "Communication Skills", "Troubleshooting", "Information Technology", "Collaborative Problem Solving", "Problem Solving", "Data Collection", "Microsoft Office", "Administrative Assistance", "Public Speaking", "Technical Troubleshooting", "Brand Ambassador", "Pre-Academic Advising", "Audio-Visual Production", "Banner System", "Resume Building", "Academic Advising", "Spreadsheets", "Hardware", "Common Sense", "Training", "Troubleshooting", "PC building", "Collaborative Problem Solving", "Information Technology Infrastructure", "Enrollment Services", "Data Collection", "Microsoft Word", "Microsoft Excel", "Microsoft Office", "Computer Hardware"]
+  }
 ];
